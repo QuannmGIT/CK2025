fe8c477c23d781e0f2596d333f2e2b23c70f3707
QuanPain <quannm1701@gmail.com>
2025-12-04 01:15:29 +0700

    Update SignInPanel.java

--- CuaHang/DangNhap/SignInPanel.java
+++ CuaHang/DangNhap/SignInPanel.java
@@ -1,4 +1,3 @@
-
 import javax.swing.*;
 import java.awt.*;
 
@@ -16,46 +15,64 @@ public class SignInPanel extends JPanel {
         title.setBounds(80, 50, 300, 40); // Căn chỉnh cho đều
         this.add(title);
 
-        // 1. Username
-        JLabel lblUser = new JLabel("Tên đăng nhập:");
-        lblUser.setFont(new Font("Segoe UI", Font.BOLD, 14));
-        lblUser.setBounds(70, 100, 150, 30); // Giữ X=70 giống bên Login
-        this.add(lblUser);
-        
-        JTextField txtUser = new JTextField();
-        txtUser.setBounds(70, 130, 250, 35);
-        txtUser.setBorder(new RoundedBorder(10));
-        this.add(txtUser);
+        // Full Name
+        JLabel SILName = new JLabel("Họ và tên:");
+        SILName.setFont(new Font("Segoe UI", Font.BOLD, 18));
+        SILName.setBounds(150, 100, 150, 30); 
+        this.add(SILName);
 
-        // 2. Email
-        JLabel lblEmail = new JLabel("Email:");
-        lblEmail.setFont(new Font("Segoe UI", Font.BOLD, 14));
-        lblEmail.setBounds(70, 170, 150, 30);
-        this.add(lblEmail);
-        
-        JTextField txtEmail = new JTextField();
-        txtEmail.setBounds(70, 200, 250, 35);
-        txtEmail.setBorder(new RoundedBorder(10));
-        this.add(txtEmail);
+        JTextField SITName = new JTextField();
+        SITName.setBounds(70, 135, 250, 35);
+        SITName.setBorder(new RoundedBorder(10));
+        this.add(SITName);        
 
-        // 3. Password
-        JLabel lblPass = new JLabel("Mật khẩu:");
-        lblPass.setFont(new Font("Segoe UI", Font.BOLD, 14));
-        lblPass.setBounds(70, 240, 150, 30);
-        this.add(lblPass);
+        // Username
+        JLabel SILUser = new JLabel("Tên đăng nhập:");
+        SILUser.setFont(new Font("Segoe UI", Font.BOLD, 18));
+        SILUser.setBounds(130, 180, 150, 30);
+        this.add(SILUser);
         
-        JPasswordField txtPass = new JPasswordField();
-        txtPass.setBounds(70, 270, 250, 35);
-        txtPass.setBorder(new RoundedBorder(10));
-        this.add(txtPass);
+        JTextField SITUser = new JTextField();
+        SITUser.setBounds(70, 215, 250, 35);
+        SITUser.setBorder(new RoundedBorder(10));
+        this.add(SITUser);
+
+        // Email
+        JLabel SILEmail = new JLabel("Email:");
+        SILEmail.setFont(new Font("Segoe UI", Font.BOLD, 18));
+        SILEmail.setBounds(165, 260, 150, 30);
+        this.add(SILEmail);
+        
+        JTextField SITEmail = new JTextField();
+        SITEmail.setBounds(70, 295, 250, 35);
+        SITEmail.setBorder(new RoundedBorder(10));
+        this.add(SITEmail);
+
+        // Password
+        JLabel SILPass = new JLabel("Mật khẩu:");
+        SILPass.setFont(new Font("Segoe UI", Font.BOLD, 18));
+        SILPass.setBounds(150, 340, 150, 30);
+        this.add(SILPass);
+        
+        JPasswordField SITPass = new JPasswordField();
+        SITPass.setBounds(70, 375, 250, 35);
+        SITPass.setBorder(new RoundedBorder(10));
+        this.add(SITPass);
+
+        
 
         // Nút Đăng ký
-        JButton btnRegister = new JButton("Đăng Ký Ngay");
-        btnRegister.setBackground(new Color(32, 178, 170)); // Màu xanh chủ đạo
-        btnRegister.setForeground(Color.WHITE);
-        btnRegister.setFont(new Font("Segoe UI", Font.BOLD, 14));
-        btnRegister.setBounds(70, 340, 250, 40);
-        this.add(btnRegister);
+        JButton DangKyBtn = new JButton("Đăng Ký Ngay");
+        DangKyBtn.setBackground(Color.WHITE);
+        DangKyBtn.setForeground(Color.BLACK);
+        DangKyBtn.setFont(new Font("Segoe UI", Font.BOLD, 14));
+        DangKyBtn.setBounds(70, 440, 250, 40);
+        this.add(DangKyBtn);
+
+        DangKyBtn.addActionListener(e -> {
+            String user = SITName.getText().trim();
+            String pass = new String(MatkhauField.getPassword()).trim();
+        );
 
         // Nút Quay lại
         JButton btnBack = new JButton("<< Quay lại Đăng Nhập");
@@ -64,13 +81,13 @@ public class SignInPanel extends JPanel {
         btnBack.setContentAreaFilled(false);
         btnBack.setForeground(Color.BLUE);
         btnBack.setCursor(new Cursor(Cursor.HAND_CURSOR));
-        btnBack.setBounds(95, 390, 200, 30);
+        btnBack.setBounds(95, 490, 200, 30);
         this.add(btnBack);
 
         // --- Logic ---
         btnBack.addActionListener(e -> mainFrame.showLoginPanel());
         
-        btnRegister.addActionListener(e -> {
+        DangKyBtn.addActionListener(e -> {
             JOptionPane.showMessageDialog(this, "Thực hiện lệnh INSERT vào DB tại đây");
         });
     }
